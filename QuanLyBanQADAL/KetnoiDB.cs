4e4a8f38e7472646906e46e4a41fcd0b8f108462
buonviyeuanh456@gmail.com <buonviyeuanh456@gmail.com>
2021-01-01 16:26:28 +0700

    third commit

---
+++ QuanLyBanQADAL/KetnoiDB.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QuanLyBanQADAL
+{
+    public class KetnoiDB
+    {
+        public static SqlConnection connect;
+        public void MoKetNoi()
+        {
+            if (KetnoiDB.connect == null)
+                KetnoiDB.connect = new SqlConnection("Data Source=DESKTOP-RK7GEUP;Initial Catalog=ClothesManager;Integrated Security=True");
+            if (KetnoiDB.connect.State != ConnectionState.Open)
+                KetnoiDB.connect.Open();
+        }
+        public void DongKetNoi()
+        {
+            if (KetnoiDB.connect != null)
+            {
+                if (KetnoiDB.connect.State == ConnectionState.Open)
+                    KetnoiDB.connect.Close();
+            }
+        }
+        public void ThucThiCauLenhSQL(string strSQl)
+        {
+            try
+            {
+                MoKetNoi();
+                SqlCommand sqlcmd = new SqlCommand(strSQl, connect);
+                sqlcmd.ExecuteNonQuery();
+                DongKetNoi();
+            }
+            catch
+            {
+
+            }
+        }
+        public DataTable GetDataTable(string strSQL)
+        {
+            try
+            {
+                MoKetNoi();
+                DataTable dt = new DataTable();
+                SqlDataAdapter sqlda = new SqlDataAdapter(strSQL, connect);
+                sqlda.Fill(dt);
+                return dt;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
--- ShopManager/Manager.Designer.cs
+++ ShopManager/Manager.Designer.cs
@@ -28,21 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.nhânViênToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kháchHàngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sảnPhẩmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hóaĐơnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thôngKêToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nhânViênToolStripMenuItem,
+            this.kháchHàngToolStripMenuItem,
+            this.sảnPhẩmToolStripMenuItem,
+            this.hóaĐơnToolStripMenuItem,
+            this.thôngKêToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(613, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // nhânViênToolStripMenuItem
+            // 
+            this.nhânViênToolStripMenuItem.Name = "nhânViênToolStripMenuItem";
+            this.nhânViênToolStripMenuItem.Size = new System.Drawing.Size(89, 24);
+            this.nhânViênToolStripMenuItem.Text = "Nhân viên";
+            // 
+            // kháchHàngToolStripMenuItem
+            // 
+            this.kháchHàngToolStripMenuItem.Name = "kháchHàngToolStripMenuItem";
+            this.kháchHàngToolStripMenuItem.Size = new System.Drawing.Size(100, 24);
+            this.kháchHàngToolStripMenuItem.Text = "Khách hàng";
+            // 
+            // sảnPhẩmToolStripMenuItem
+            // 
+            this.sảnPhẩmToolStripMenuItem.Name = "sảnPhẩmToolStripMenuItem";
+            this.sảnPhẩmToolStripMenuItem.Size = new System.Drawing.Size(89, 24);
+            this.sảnPhẩmToolStripMenuItem.Text = "Sản phẩm";
+            // 
+            // hóaĐơnToolStripMenuItem
+            // 
+            this.hóaĐơnToolStripMenuItem.Name = "hóaĐơnToolStripMenuItem";
+            this.hóaĐơnToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.hóaĐơnToolStripMenuItem.Text = "Hóa đơn";
+            // 
+            // thôngKêToolStripMenuItem
+            // 
+            this.thôngKêToolStripMenuItem.Name = "thôngKêToolStripMenuItem";
+            this.thôngKêToolStripMenuItem.Size = new System.Drawing.Size(84, 24);
+            this.thôngKêToolStripMenuItem.Text = "Thông kê";
             // 
             // frmManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(613, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmManager";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Manager";
             this.Load += new System.EventHandler(this.frmManager_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nhânViênToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kháchHàngToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sảnPhẩmToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hóaĐơnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thôngKêToolStripMenuItem;
     }
 }
