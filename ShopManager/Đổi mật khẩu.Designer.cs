dc380a771e450661f7a9f6fc0708e377c097c32b
SoullessEye <buonviyeuanh456@gmail.com>
2021-01-06 21:34:21 +0700

    doimatkhau

---
+++ ShopManager/Đổi mật khẩu.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ShopManager
+{
+    partial class frmDoimatkhau
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTendangnhap = new System.Windows.Forms.TextBox();
+            this.txtMatkhaucu = new System.Windows.Forms.TextBox();
+            this.txtMatkhaumoi = new System.Windows.Forms.TextBox();
+            this.txtXacnhan = new System.Windows.Forms.TextBox();
+            this.btnDongY = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên đăng nhập";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mật khẩu cũ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Mật khẩu mới";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Xác nhận mật khẩu";
+            // 
+            // txtTendangnhap
+            // 
+            this.txtTendangnhap.Location = new System.Drawing.Point(183, 61);
+            this.txtTendangnhap.Name = "txtTendangnhap";
+            this.txtTendangnhap.Size = new System.Drawing.Size(253, 22);
+            this.txtTendangnhap.TabIndex = 1;
+            // 
+            // txtMatkhaucu
+            // 
+            this.txtMatkhaucu.Location = new System.Drawing.Point(183, 110);
+            this.txtMatkhaucu.Name = "txtMatkhaucu";
+            this.txtMatkhaucu.Size = new System.Drawing.Size(253, 22);
+            this.txtMatkhaucu.TabIndex = 2;
+            // 
+            // txtMatkhaumoi
+            // 
+            this.txtMatkhaumoi.Location = new System.Drawing.Point(183, 161);
+            this.txtMatkhaumoi.Name = "txtMatkhaumoi";
+            this.txtMatkhaumoi.Size = new System.Drawing.Size(253, 22);
+            this.txtMatkhaumoi.TabIndex = 3;
+            // 
+            // txtXacnhan
+            // 
+            this.txtXacnhan.Location = new System.Drawing.Point(183, 213);
+            this.txtXacnhan.Name = "txtXacnhan";
+            this.txtXacnhan.Size = new System.Drawing.Size(253, 22);
+            this.txtXacnhan.TabIndex = 4;
+            // 
+            // btnDongY
+            // 
+            this.btnDongY.Location = new System.Drawing.Point(183, 302);
+            this.btnDongY.Name = "btnDongY";
+            this.btnDongY.Size = new System.Drawing.Size(82, 36);
+            this.btnDongY.TabIndex = 5;
+            this.btnDongY.Text = "Đồng ý";
+            this.btnDongY.UseVisualStyleBackColor = true;
+            this.btnDongY.Click += new System.EventHandler(this.btnDongY_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(361, 302);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 36);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Thoát";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmDoimatkhau
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnDongY);
+            this.Controls.Add(this.txtXacnhan);
+            this.Controls.Add(this.txtMatkhaumoi);
+            this.Controls.Add(this.txtMatkhaucu);
+            this.Controls.Add(this.txtTendangnhap);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDoimatkhau";
+            this.Text = "Đổi_mật_khẩu";
+            this.Load += new System.EventHandler(this.frmDoimatkhau_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTendangnhap;
+        private System.Windows.Forms.TextBox txtMatkhaucu;
+        private System.Windows.Forms.TextBox txtMatkhaumoi;
+        private System.Windows.Forms.TextBox txtXacnhan;
+        private System.Windows.Forms.Button btnDongY;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ ShopManager/Đổi mật khẩu.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ShopManager
+{
+    public partial class frmDoimatkhau : Form
+    {
+        string strConnection = @"Data Source=DESKTOP-RK7GEUP;Initial Catalog=ClothesManager;Integrated Security=True";
+        SqlConnection conn;
+        public frmDoimatkhau()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDoimatkhau_Load(object sender, EventArgs e)
+        {
+            if (conn == null)
+                conn = new SqlConnection(strConnection);
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+            SqlCommand comand = new SqlCommand();
+            comand.CommandType = CommandType.Text;
+            comand.CommandText = "select*from Account where Ma = 1";
+            comand.Connection = conn;
+            SqlDataReader reader = comand.ExecuteReader();
+            if (reader.Read())
+            {
+                txtTendangnhap.Text = reader.GetString(1).Trim();
+            }
+            reader.Close();
+        }
+
+        private void btnDongY_Click(object sender, EventArgs e)
+        {
+            if (conn == null)
+                conn = new SqlConnection(strConnection);
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "select*from Account where Ma=1";
+            command.Connection = conn;
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                if (txtMatkhaucu.Text == reader.GetString(2).Trim())
+                {
+                    reader.Close();
+                    if (txtMatkhaumoi.Text == txtXacnhan.Text)
+                    {
+                        if (conn == null)
+                            conn = new SqlConnection(strConnection);
+                        if (conn.State == ConnectionState.Closed)
+                            conn.Open();
+                        command = new SqlCommand();
+                        command.CommandType = CommandType.Text;
+                        command.CommandText = "update Account set Matkhau=@matkhau where Ma=1";
+                        command.Connection = conn;
+                        command.Parameters.Add("@matkhau", SqlDbType.NVarChar).Value = txtMatkhaumoi.Text;
+                        int ret = command.ExecuteNonQuery();
+                        if(ret>0)
+                        {
+                            MessageBox.Show("Cập nhật mật khẩu thành công");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Cập nhật mật khẩu thất bại");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Xác nhận mật khẩu chưa chính xác");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Bạn nhập mật khẩu cũ chưa chính xác");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
