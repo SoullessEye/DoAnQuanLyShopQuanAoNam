71524d17dc161ee1589a64f220799d5531093a1f
hoaa52019 <76459307+hoaa52019@users.noreply.github.com>
2021-01-06 20:32:14 +0700

    hoadon

---
+++ ShopManager/frmHoaDon.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace ShopManager
+{
+    partial class frmHoaDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgvHoadon = new System.Windows.Forms.DataGridView();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMaHD = new System.Windows.Forms.TextBox();
+            this.cmbMakh = new System.Windows.Forms.ComboBox();
+            this.cmbMaNV = new System.Windows.Forms.ComboBox();
+            this.clothesManagerDataSet2 = new ShopManager.ClothesManagerDataSet2();
+            this.hoadonBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.hoadonTableAdapter = new ShopManager.ClothesManagerDataSet2TableAdapters.HoadonTableAdapter();
+            this.maHDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ngayLapDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.makhDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maNVDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateTimeNgaylap = new System.Windows.Forms.DateTimePicker();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHoadon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clothesManagerDataSet2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hoadonBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dateTimeNgaylap);
+            this.panel1.Controls.Add(this.cmbMaNV);
+            this.panel1.Controls.Add(this.cmbMakh);
+            this.panel1.Controls.Add(this.txtMaHD);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 120);
+            this.panel1.TabIndex = 0;
+            // 
+            // dgvHoadon
+            // 
+            this.dgvHoadon.AutoGenerateColumns = false;
+            this.dgvHoadon.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvHoadon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHoadon.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.maHDDataGridViewTextBoxColumn,
+            this.ngayLapDataGridViewTextBoxColumn,
+            this.makhDataGridViewTextBoxColumn,
+            this.maNVDataGridViewTextBoxColumn});
+            this.dgvHoadon.DataSource = this.hoadonBindingSource;
+            this.dgvHoadon.Location = new System.Drawing.Point(12, 156);
+            this.dgvHoadon.Name = "dgvHoadon";
+            this.dgvHoadon.RowHeadersWidth = 51;
+            this.dgvHoadon.RowTemplate.Height = 24;
+            this.dgvHoadon.Size = new System.Drawing.Size(776, 243);
+            this.dgvHoadon.TabIndex = 1;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(12, 431);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 2;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(165, 431);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 2;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(296, 431);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(432, 431);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 23);
+            this.btnLuu.TabIndex = 2;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Location = new System.Drawing.Point(568, 431);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(75, 23);
+            this.btnHuy.TabIndex = 2;
+            this.btnHuy.Text = "Hủy bỏ";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(713, 431);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 2;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã hóa đơn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ngày lập";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(417, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Mã khách hàng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(417, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Mã nhân viên";
+            // 
+            // txtMaHD
+            // 
+            this.txtMaHD.Location = new System.Drawing.Point(106, 16);
+            this.txtMaHD.Name = "txtMaHD";
+            this.txtMaHD.Size = new System.Drawing.Size(181, 22);
+            this.txtMaHD.TabIndex = 1;
+            // 
+            // cmbMakh
+            // 
+            this.cmbMakh.FormattingEnabled = true;
+            this.cmbMakh.Location = new System.Drawing.Point(528, 19);
+            this.cmbMakh.Name = "cmbMakh";
+            this.cmbMakh.Size = new System.Drawing.Size(188, 24);
+            this.cmbMakh.TabIndex = 2;
+            // 
+            // cmbMaNV
+            // 
+            this.cmbMaNV.FormattingEnabled = true;
+            this.cmbMaNV.Location = new System.Drawing.Point(528, 69);
+            this.cmbMaNV.Name = "cmbMaNV";
+            this.cmbMaNV.Size = new System.Drawing.Size(188, 24);
+            this.cmbMaNV.TabIndex = 2;
+            // 
+            // clothesManagerDataSet2
+            // 
+            this.clothesManagerDataSet2.DataSetName = "ClothesManagerDataSet2";
+            this.clothesManagerDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // hoadonBindingSource
+            // 
+            this.hoadonBindingSource.DataMember = "Hoadon";
+            this.hoadonBindingSource.DataSource = this.clothesManagerDataSet2;
+            // 
+            // hoadonTableAdapter
+            // 
+            this.hoadonTableAdapter.ClearBeforeFill = true;
+            // 
+            // maHDDataGridViewTextBoxColumn
+            // 
+            this.maHDDataGridViewTextBoxColumn.DataPropertyName = "MaHD";
+            this.maHDDataGridViewTextBoxColumn.HeaderText = "MaHD";
+            this.maHDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.maHDDataGridViewTextBoxColumn.Name = "maHDDataGridViewTextBoxColumn";
+            // 
+            // ngayLapDataGridViewTextBoxColumn
+            // 
+            this.ngayLapDataGridViewTextBoxColumn.DataPropertyName = "NgayLap";
+            this.ngayLapDataGridViewTextBoxColumn.HeaderText = "NgayLap";
+            this.ngayLapDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.ngayLapDataGridViewTextBoxColumn.Name = "ngayLapDataGridViewTextBoxColumn";
+            // 
+            // makhDataGridViewTextBoxColumn
+            // 
+            this.makhDataGridViewTextBoxColumn.DataPropertyName = "Makh";
+            this.makhDataGridViewTextBoxColumn.HeaderText = "Makh";
+            this.makhDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.makhDataGridViewTextBoxColumn.Name = "makhDataGridViewTextBoxColumn";
+            // 
+            // maNVDataGridViewTextBoxColumn
+            // 
+            this.maNVDataGridViewTextBoxColumn.DataPropertyName = "MaNV";
+            this.maNVDataGridViewTextBoxColumn.HeaderText = "MaNV";
+            this.maNVDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.maNVDataGridViewTextBoxColumn.Name = "maNVDataGridViewTextBoxColumn";
+            // 
+            // dateTimeNgaylap
+            // 
+            this.dateTimeNgaylap.Location = new System.Drawing.Point(106, 64);
+            this.dateTimeNgaylap.Name = "dateTimeNgaylap";
+            this.dateTimeNgaylap.Size = new System.Drawing.Size(200, 22);
+            this.dateTimeNgaylap.TabIndex = 3;
+            // 
+            // frmHoaDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 476);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.dgvHoadon);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmHoaDon";
+            this.Text = "frmHoaDon";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmHoaDon_FormClosing);
+            this.Load += new System.EventHandler(this.frmHoaDon_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHoadon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clothesManagerDataSet2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hoadonBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgvHoadon;
+        private System.Windows.Forms.ComboBox cmbMaNV;
+        private System.Windows.Forms.ComboBox cmbMakh;
+        private System.Windows.Forms.TextBox txtMaHD;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Button btnThoat;
+        private ClothesManagerDataSet2 clothesManagerDataSet2;
+        private System.Windows.Forms.BindingSource hoadonBindingSource;
+        private ClothesManagerDataSet2TableAdapters.HoadonTableAdapter hoadonTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maHDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ngayLapDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn makhDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maNVDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DateTimePicker dateTimeNgaylap;
+    }
+}
---
+++ ShopManager/frmHoaDon.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ShopManager
+{
+    public partial class frmHoaDon : Form
+    {
+        public frmHoaDon()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = null;
+        string strConn = @"Data Source=DESKTOP-RK7GEUP;Initial Catalog=ClothesManager;Integrated Security=True";
+        SqlDataAdapter daNhanvien = null;
+        DataTable dtNhanvien = null;
+        SqlDataAdapter daKhachhang = null;
+        DataTable dtKhachhang = null;
+        SqlDataAdapter daHoadon = null;
+        DataTable dtHoadon = null;
+        bool Them;
+        private void frmHoaDon_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'clothesManagerDataSet2.Hoadon' table. You can move, or remove it, as needed.
+            //this.hoadonTableAdapter.Fill(this.clothesManagerDataSet2.Hoadon);
+            LoadData();
+        }
+        void LoadData()
+        {
+            try
+            {
+                if (conn == null)
+                    conn = new SqlConnection(strConn);
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+                daHoadon = new SqlDataAdapter("Select*from Hoadon", conn);
+                dtHoadon = new DataTable();
+                dtHoadon.Clear();
+                daHoadon.Fill(dtHoadon);
+                dgvHoadon.DataSource = dtHoadon;
+                dgvHoadon.AutoResizeColumns();
+                daKhachhang = new SqlDataAdapter("Select Makh from Khachhang", conn);
+                dtKhachhang = new DataTable();
+                dtKhachhang.Clear();
+                daKhachhang.Fill(dtKhachhang);
+                daNhanvien = new SqlDataAdapter("Select MaNV from Nhanvienn", conn);
+                dtNhanvien = new DataTable();
+                dtNhanvien.Clear();
+                daNhanvien.Fill(dtNhanvien);
+                txtMaHD.Text = "";
+                dateTimeNgaylap.Text = "";
+
+                btnLuu.Enabled = false;
+                btnHuy.Enabled = false;
+                panel1.Enabled = false;
+
+                btnThem.Enabled = true;
+                btnSua.Enabled = true;
+                btnXoa.Enabled = true;
+                btnThoat.Enabled = true;
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Loi !!!");
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            Them = true;
+            txtMaHD.Text = "";
+            dateTimeNgaylap.Text = "";
+            this.btnLuu.Enabled = true;
+            this.btnHuy.Enabled = true;
+            this.panel1.Enabled = true;
+
+            this.btnThem.Enabled = false;
+            this.btnSua.Enabled = false;
+            this.btnXoa.Enabled = false;
+            this.btnThoat.Enabled = false;
+            cmbMakh.DataSource = dtKhachhang;
+            cmbMakh.ValueMember = "Makh";
+            cmbMakh.DisplayMember = "Makh";
+            cmbMaNV.DataSource = dtNhanvien;
+            cmbMaNV.ValueMember = "MaNV";
+            cmbMaNV.DisplayMember = "MaNV"; 
+            txtMaHD.Focus();
+        }
+
+        private void frmHoaDon_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            dtHoadon.Dispose();
+            dtHoadon = null;
+            conn = null;
+            dtKhachhang.Dispose();
+            dtKhachhang = null;
+            dtNhanvien.Dispose();
+            dtNhanvien = null;
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            Them = false;
+            cmbMakh.DataSource = dtKhachhang;
+            cmbMakh.ValueMember = "Makh";
+            cmbMakh.DisplayMember = "Makh";
+            cmbMaNV.DataSource = dtNhanvien;
+            cmbMaNV.ValueMember = "MaNV";
+            cmbMaNV.DisplayMember = "MaNV";
+
+            panel1.Enabled = true;
+            int r = dgvHoadon.CurrentCell.RowIndex;
+            txtMaHD.Text = dgvHoadon.Rows[r].Cells[0].Value.ToString();
+            dateTimeNgaylap.Text = dgvHoadon.Rows[r].Cells[1].Value.ToString();
+            cmbMakh.SelectedValue = dgvHoadon.Rows[r].Cells[2].Value.ToString();
+            cmbMaNV.SelectedValue = dgvHoadon.Rows[r].Cells[3].Value.ToString();
+            
+
+            btnLuu.Enabled = true;
+            btnHuy.Enabled = true;
+            panel1.Enabled = true;
+            btnThem.Enabled = false;
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
+            btnThoat.Enabled = false;
+            txtMaHD.Focus();
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+
+            if (Them)
+            {
+                try
+                {
+                    SqlCommand command = new SqlCommand();
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "insert into Hoadon values(@MaHD, @NgayLap, @Makh, @MaNV)";
+                    command.Connection = conn;
+                    command.Parameters.Add("@MaHD", SqlDbType.NVarChar).Value = txtMaHD.Text;
+                    command.Parameters.Add("@NgayLap", SqlDbType.Date).Value = dateTimeNgaylap.Text;
+                    command.Parameters.Add("@Makh", SqlDbType.NVarChar).Value = cmbMakh.SelectedValue;
+                    command.Parameters.Add("@MaNV", SqlDbType.NVarChar).Value = cmbMaNV.SelectedValue;
+                    command.ExecuteNonQuery();
+                    LoadData();
+                    MessageBox.Show("Đã thêm xong");
+                }
+                catch (SqlException)
+                {
+                    MessageBox.Show("không thêm được, lỗi !!!");
+                }
+            }
+            if (!Them)
+            {
+                try
+                {
+                    SqlCommand command = new SqlCommand();
+                    command.CommandType = CommandType.Text;
+                    int r = dgvHoadon.CurrentCell.RowIndex;
+                    string MaHD = dgvHoadon.Rows[r].Cells[0].Value.ToString();
+                    command.CommandText = "update Hoadon set NgayLap=@NgayLap, Makh=@Makh, MaNV=@MaNV where MaHD=@MaHD";
+                    command.Connection = conn;
+
+                    command.Parameters.Add("@NgayLap", SqlDbType.Date).Value = dateTimeNgaylap.Text;
+                    command.Parameters.Add("@Makh", SqlDbType.NVarChar).Value = cmbMakh.SelectedValue;
+                    command.Parameters.Add("@MaNV", SqlDbType.NVarChar).Value = cmbMaNV.SelectedValue;
+                    command.Parameters.Add("@MaHD", SqlDbType.NVarChar).Value = MaHD;
+                    command.ExecuteNonQuery();
+                    LoadData();
+                    MessageBox.Show("Đã cập nhật xong");
+                }
+                catch (SqlException)
+                {
+                    MessageBox.Show("không cập nhật được, lỗi !!!");
+                }
+            }
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            txtMaHD.Text = "";
+            dateTimeNgaylap.Text = "";
+
+            btnLuu.Enabled = false;
+            btnHuy.Enabled = false;
+            panel1.Enabled = false;
+            btnThem.Enabled = true;
+            btnSua.Enabled = true;
+            btnXoa.Enabled = true;
+            btnThoat.Enabled = true;
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.CommandType = CommandType.Text;
+                int r = dgvHoadon.CurrentCell.RowIndex;
+                string MaHD = dgvHoadon.Rows[r].Cells[0].Value.ToString();
+                command.CommandText = "delete from Hoadon where MaHD=@MaHD";
+                command.Connection = conn;
+                command.Parameters.Add("@MaHD", SqlDbType.NVarChar).Value = MaHD;
+                command.ExecuteNonQuery();
+                LoadData();
+                MessageBox.Show("Xóa xong");
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Không xóa được, lỗi");
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
